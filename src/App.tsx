7f0fa1d9b5a9cb1d261a46be3e22b577771c6f14
Alessandro Muratore <muratore@gmail.com>
2023-04-12 11:15:50 -0300

    Desafio seção 3 - Avançando em React

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+    <h1>Avançando em React</h1>
+    </div>
+  )
+}
+
+export default App
